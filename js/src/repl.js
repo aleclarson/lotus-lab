9b705e66a932aa2f6d907495fe9fb550accc2762
aleclarsoniv <alec.stanford.larson@gmail.com>
2016-05-09 17:31:56 -0700

    05/09/2016 5:31 PM

--- js/src/repl.js
+++ js/src/repl.js
@@ -1,4 +1,10 @@
-module.exports = function() {
+module.exports = function(options) {
+  repl.transform = options.transform != null ? options.transform : options.transform = process.env.REPL || "js";
+  repl.loopMode = options.loopMode != null ? options.loopMode : options.loopMode = "nextTick";
+  repl.didClose.once(function() {
+    log.it("repl.didClose()");
+    return process.exit();
+  });
   return repl.sync();
 };
 
--- src/repl.coffee
+++ src/repl.coffee
@@ -1,3 +1,8 @@
 
-module.exports = ->
+module.exports = (options) ->
+  repl.transform = options.transform ?= process.env.REPL or "js"
+  repl.loopMode = options.loopMode ?= "nextTick"
+  repl.didClose.once ->
+    log.it "repl.didClose()"
+    process.exit()
   repl.sync()
